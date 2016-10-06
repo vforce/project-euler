7b6aeab73ba5a95a65f4df29c9f0e30c5942f4c2
wakandan <wakandan@gmail.com>
2016-10-06 09:47:38 +0700

    update problem 31 to use dp

--- 31_coin_sums.rb
+++ 31_coin_sums.rb
@@ -1,16 +1,24 @@
-A = [200, 100, 50, 20, 10, 5, 2, 1]
-$a = 200
-$count = 0
-def try(i, amount)
-  if i==A.size-1
-    $count += 1
-    return
+coins = [200, 100, 50, 20, 10, 5, 2, 1]
+next_coins = {}
+coins.each do |c|
+  if c!=1
+    next_coins[c] = coins[coins.index(c)+1]
   end
-
-  (0..amount/A[i]).each do |x|
-    try(i+1, amount - x*A[i])
+end
+coins_reverse = coins.reverse
+n = 10000
+A = Array.new(n+1) {{}}
+(0..n).each do |i|
+  coins_reverse.each do |c|
+    A[i][c] = 0
+    if c==1 || i==0
+      A[i][c] = 1
+    else
+      (0..i/c).each do |j|
+        A[i][c] += A[i-j*c][next_coins[c]]
+      end
+    end
   end
 end
 
-try(0, $a)
-puts $count
+puts A[n][200]
