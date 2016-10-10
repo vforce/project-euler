65414d71276f3cf5a66a4de2abe7e11564793d0b
Dang Nguyen Anh Khoa <wakandan@gmail.com>
2016-10-10 08:00:42 +0700

    update problem 37

--- 37_truncatable_primes.rb
+++ 37_truncatable_primes.rb
@@ -1,4 +1,5 @@
-n = 1000
+require 'pp'
+n = 1000000
 cross_limit = Math.sqrt(n).floor
 sieve_bound = n/2-1
 sieve = Array.new(sieve_bound, true)
@@ -13,6 +14,7 @@ sieve = Array.new(sieve_bound, true)
   end
 end
 
+
 primes = {2 => true}
 sieve.each_with_index do |v, i|
   if v and i>0
@@ -21,21 +23,39 @@ sieve.each_with_index do |v, i|
 end
 
 numbers = []
-(2,3,5,7,9).each do |k|
+count = 0
+init = false
+sum = 0
+while count < 11
   last = numbers.shift
   if last.nil?
-    last << k
+    [2,3,5,7,9].each do |k|
+      numbers << k
+    end
+    break if init
+    init = true
   else
+    is_prime_a = false
+    is_prime_b = false
     m = last
     while m>0
       break if !primes[m]
       m = m/10
     end
-    next if m > 0
-    m = last
-    while m > 0
-      break if !primes[m]
-      m = m.chars.shift.to_i
+    is_prime_a = true if m==0
+    m = last.to_s.chars
+    while m.size > 0
+      break if !primes[m.join.to_i]
+      m.shift 
+    end
+    is_prime_b = true if m.size==0
+    if is_prime_a && is_prime_b and last > 10
+      puts last
+      sum += last
+      count += 1
+    end
+    [2,3,5,7,9].each do |k|
+      numbers <<  last * 10 + k
     end
   end
 end
