6708d7a3aeaaef0711826ea4b31d4d03dc13227a
wakandan <wakandan@gmail.com>
2017-01-25 15:11:33 +0700

    update linked_list code and test

--- spoj/linked_list.rb
+++ spoj/linked_list.rb
@@ -25,6 +25,121 @@ class Node
     result.strip
   end
 
+  def partition(x)
+    node = self
+    head_less = Node.new(0)
+    head_more = Node.new(0)
+    while node!=nil
+      if node.data<x
+        head_less.append(Node.new(node.data))    
+      else
+        head_more.append(Node.new(node.data))
+      end
+      node = node.next
+    end
+    head_more = head_more.next
+    head_less = head_less.next
+    head_less.append(head_more)
+    head_less
+  end
+
+  def partition!(x)
+    node_more = head_more = Node.new(0)
+    node_less = head_less = Node.new(0)
+    node = self
+    while node!=nil
+      if node.data<x
+        node_less.next = node
+        node_less = node
+      else
+        node_more.next = node
+        node_more = node
+      end
+      node = node.next
+    end
+    head_less = head_less.next
+    head_more = head_more.next
+    node_less.next = head_more
+    return head_less
+  end
+
+  def reverse
+    node = nex = self
+    prev = nil
+    while node!=nil
+      nex = node.next
+      node.next = prev
+      prev = node
+      if nex == nil
+        return node
+      end
+      node = nex
+    end
+  end
+
+  def reverse_sum(other)
+    (self.reverse + other.reverse).reverse
+  end
+
+
+  def +(other)
+    return self if other.nil?
+    carry = 0
+    node_other = other
+    node = self
+    head_res = res = Node.new(0)
+    while true
+      if node.nil? && node_other.nil?
+        if carry > 0
+          res.next = Node.new(carry) 
+        end
+        return head_res.next
+      end
+      s = (node.nil? ? 0  : node.data) +
+        (node_other.nil? ? 0 : node_other.data) +
+        carry
+      carry = s/10
+      res.next = Node.new(s%10)
+      res = res.next
+      node = node.next if node!=nil
+      node_other = node_other.next if node_other!=nil
+    end
+  end
+
+  #1 2 3 4 5  => 3 (remains 1 2 4 5)
+  #1 2 3 4 5 6 => 4 (remains 1 2 3 5 6)
+  def remove_middle
+    prev = head = node = self
+    while node!=nil
+      if node.next.nil?
+        break
+      end
+      prev = head
+      head = head.next
+      node = node.next.next
+    end
+    prev.next = head.next
+    head.data
+  end
+
+  #find the kth element from last
+  def find_k_element(k)
+    return self if k<1 
+    node = head = self
+    (1..k-1).each do |i|
+      if node.nil?
+        return nil
+      else
+        node = node.next
+      end
+    end
+    while node.next!=nil
+      head = head.next
+      node = node.next
+    end
+    return head
+  end
+
   def remove_unsorted_duplicated_2
     head = self
     while head!=nil
@@ -34,7 +149,7 @@ class Node
         if head.data==node.data
           prev.next = node.next
         else
-          prev = prev.next
+          prev = prev.next #only move prev if node is not removed
         end
         node = node.next
       end
@@ -49,7 +164,7 @@ class Node
     while node!=nil && node.next != nil
       d = node.next.data
       if values[d] != nil
-        node.next = node.next.next
+        node.next = node.next.next #do not move node if it's not removed
       else
         values[d] = true
         node = node.next
--- spoj/linked_list_spec.rb
+++ spoj/linked_list_spec.rb
@@ -4,6 +4,15 @@ describe 'linked list should work correctly' do
   def node
     @node ||= Node.new(1)
   end
+  def from_list
+    return Node.from_list(list.map {|x| x.to_i})
+  end
+  def from_str(list)
+    return Node.from_list(list.split.map {|x| x.to_i})
+  end
+  def sum_str(str1, str2)
+    return from_str(str1) + from_str(str2)
+  end
   it 'should initialize correctly' do
     expect(node.next).to be_nil
     expect(node.data).to be(1)
@@ -37,6 +46,24 @@ describe 'linked list should work correctly' do
     expect(node.size).to eq(4)
   end
 
+  it 'should locate the kth element from last correctly' do
+    node = Node.from_list(%w[1 2 3 4 3 4 2])
+    expect(node.find_k_element(1).data).to eq('2')
+    expect(node.find_k_element(4).data).to eq('4')
+    expect(node.find_k_element(100)).to be_nil
+  end
+
+  it 'should remove the middle element' do
+    node = Node.from_list(%w[1 2 3 4 5])
+    expect(node.remove_middle).to eq('3')
+    expect(node.size).to eq(4)
+    expect(node.next.next.data).to eq('4')
+    node = Node.from_list(%w[1 2 3 4 5 6])
+    expect(node.remove_middle).to eq('4')
+    expect(node.size).to eq(5)
+    expect(node.next.next.next.data).to eq('5')
+  end
+
   it 'remove unsorted linked list' do
     node = Node.from_list(%w[1 1 1])
     node.remove_unsorted_duplicated_1
@@ -44,6 +71,32 @@ describe 'linked list should work correctly' do
     node = Node.from_list(%w[1 2 3 4 3 4 2])
     node.remove_unsorted_duplicated_1
     expect(node.size).to eq(4)
+  end
+
+  it 'should partition correctly' do
+    expect(from_str('3 5 2 1 6').partition(4).to_s).to eq('3 2 1 5 6')
+  end
+
+  it 'should partition! correctly' do
+    expect(Node.new(1).partition!(4).to_s).to eq('1')
+    expect(from_str('3 5 2 1 6').partition!(4).to_s).to eq('3 2 1 5 6')
+  end
+
+  it 'should reverse correctly' do
+    expect(from_str('1 2 3').reverse.to_s).to eq('3 2 1')
+  end
+
+  it 'should do sum in normal order correctly' do
+    expect(from_str('6 1 7').reverse_sum(from_str('2 9 5')).to_s).to \
+      eq('9 1 2')
+    expect(from_str('1 1').reverse_sum(from_str('9 9 9')).to_s).to \
+      eq('1 0 1 0')
+  end
+
+  it 'should do sum correctly when saved in reversed order' do
+    expect(sum_str('7 1 6', '5 9 2').to_s).to eq('2 1 9')
+    expect(sum_str('1 1', '9 9 9').to_s).to eq('0 1 0 1')
+    expect(sum_str('0', '9 9 9').to_s).to eq('9 9 9')
   end
 
   it 'should append correctly' do
