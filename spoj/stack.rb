729f85bcf697c42e2b58de4a87e6c63a11af7f64
wakandan <wakandan@gmail.com>
2017-01-31 11:37:08 +0700

    add stack

---
+++ spoj/stack.rb
@@ -0,0 +1,46 @@
+require './linked_list'
+
+class Stack
+  def initialize
+    @node = nil
+  end
+
+  def push(x)
+    if @node == nil
+      @node = Node.new(x)
+    else
+      @node.next = Node.new(x)
+    end
+    self
+  end
+
+  def pop
+    return nil if @node.nil?
+    if @node.next.nil?
+      data = @node.data
+      @node = nil
+      return data
+    else
+      prev = node = @node
+      while node.next != nil
+        prev = node
+        node = node.next
+      end
+      prev.next = nil
+      node.data
+    end
+  end
+
+  def peek
+    return nil if @node.nil?
+    node = @node
+    while node.next!=nil
+      node = node.next
+    end
+    node.data
+  end
+
+  def size
+    @node.nil? ? 0: @node.size
+  end
+end
---
+++ spoj/stack_spec.rb
@@ -0,0 +1,45 @@
+require './stack'
+
+describe 'stack' do
+  def stack
+    @stack ||= Stack.new
+  end
+
+  it 'should init' do
+    stack = Stack.new
+  end
+
+  it 'size = 0 when empty' do
+    expect(Stack.new.size).to eq(0)
+  end
+
+  it 'should push' do
+    stack.push(1)
+    expect(stack.size).to eq(1)
+  end
+
+  describe 'should peek' do
+    it 'should return nil when empty' do
+      expect(stack.peek).to be_nil
+    end
+    it 'should return top data' do
+      stack.push(1)
+      stack.push(3)
+      expect(stack.peek).to eq(3)
+    end
+  end
+
+  describe 'should pop' do
+    it 'should return nil when empty' do
+      expect(stack.pop).to be_nil
+    end
+    it 'should be empty once popped everything' do
+      expect(stack.push(1).pop).to eq(1)
+      expect(stack.size).to eq(0)
+    end
+    it 'should reduce stack size by 1' do
+      expect(stack.push(1).push(2).pop).to eq(2)
+      expect(stack.size).to eq(1)
+    end
+  end
+end
